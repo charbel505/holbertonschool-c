959ebe54a20a82c2b8acbeb760c72ab4897d4286
charbel505 <charbeltannous505@gmail.com>
2021-08-02 19:44:12 +0000

    fixing the coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-  char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, sentence, 59);
-  return (1);
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, sentence, 59);
+	return (1);
 }
