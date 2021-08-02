70d235e80dc0bfabd37dc29ce123c906562aee79
charbel505 <charbeltannous505@gmail.com>
2021-08-02 19:13:05 +0000

    writing the codes using the C coding style of Holberton

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
-int main(){
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
   char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
   write(2, sentence, 59);
   return (1);
