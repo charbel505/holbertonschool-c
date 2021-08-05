78af3c5597c71ff50541ee9f570358cbbf215738
charbel505 <charbeltannous505@gmail.com>
2021-08-05 18:42:03 +0000

    fixing code style bugs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 /**
- * print_last_digit: last digit printing
- * 
+ * print_last_digit - print the last digit
+ * @i: integer
  *
- * 
+ * Return: last digit of i
  */
 int print_last_digit(int i)
 {
-	int lastd = i % 10;
+	int last_digit = i % 10;
 
-	if (lastd < 0)
-		lastd = last_digit * -1;
-	_putchar(lastd + '0');
-	return (lastd);
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
