6f85d87921e547cb1a4ae405a43d8411f707bca7
charbel505 <charbeltannous505@gmail.com>
2021-08-18 18:40:20 +0000

    rest of the tasks

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,78 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * word_count - function counts amount of words
+ * seperated by space
+ * @s: string to check
+ *
+ * Return: number of words
+ */
+int word_count(char *s)
+{
+	int c = 0, i = 0;
+
+	while (s[i])
+	{
+		if (s[i] != ' ')
+		{
+			c++;
+			while (s[i] && s[i] != ' ')
+				i++;
+		}
+		else
+			i++;
+	}
+	return (c);
+}
+
+/**
+ * strtow - function splits a string into words
+ * @str: string to split up
+ *
+ * Return: a pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	char **a, *tmpP;
+	int i = 0, j = 0, pos = 0, size = 0, words, tmp;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	words = word_count(str);
+	if (words == 0)
+		return (NULL);
+	a = malloc(sizeof(char *) * (words + 1));
+	if (!a)
+		return (NULL);
+	j = 0;
+	for (i = 0; i < words; i++)
+	{
+		size = 0;
+		while (str[pos] == ' ')
+			pos++;
+		tmp = pos;
+		while (str[pos] && str[pos] != ' ')
+		{
+			size++;
+			pos++;
+		}
+
+		tmpP = malloc(sizeof(char) * (size + 1));
+		if (!tmpP)
+		{
+			free(tmpP);
+			return (NULL);
+		}
+		for (j = 0; j < size; j++)
+		{
+			tmpP[j] = str[tmp];
+			tmp++;
+		}
+		tmpP[j] = '\0';
+		a[i] = tmpP;
+	}
+	a[i] = NULL;
+	return (a);
+}
