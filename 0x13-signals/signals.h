cd08df80b19f0b4c84dc4139009a485305e91352
charbel505 <charbeltannous505@gmail.com>
2021-10-10 20:46:41 +0000

    adding header file

---
+++ 0x13-signals/signals.h
@@ -0,0 +1,25 @@
+#ifndef SIGNALS_H
+#define SIGNALS_H
+
+#define _GNU_SOURCE
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int handle_signal(void);
+void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
+void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
+int pid_exist(pid_t pid);
+void all_in_one(void);
+int sigset_init(sigset_t *set, int *signals);
+int signals_block(int *signals);
+int signals_unblock(int *signals);
+int handle_pending(void (*handler)(int));
+
+#endif
